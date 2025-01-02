d98dd6d2c38209e2a50032a6675d02c2377c762d
W4v3-cmd <charlymayeur3@gmail.com>
2025-01-02 23:37:41 +0100

    index script style update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", () => {
     const logoContainer = document.querySelector(".main-intro");
     const textElement = document.querySelector(".hidden-text");
+    const textBvn = document.querySelector(".bienvenue-txt");
+    
     const searchBarContainer = document.querySelector(".search-bar-container");
 
     if (window.scrollY > 30) {
@@ -8,10 +10,12 @@ window.addEventListener("scroll", () => {
         logoContainer.classList.add("logo-small");
         // Fixer le conteneur sous le logo
         searchBarContainer.classList.add("fixed-container");
+        textBvn.classList.add("rmvBvn");
     } else {
         // Restaurer la position initiale du logo
         logoContainer.classList.remove("logo-small");
         // Restaurer la position initiale du conteneur
         searchBarContainer.classList.remove("fixed-container");
+        textBvn.classList.remove("rmvBvn");
     }
 });
