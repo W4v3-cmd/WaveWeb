47b221476b69b9cb3bd17a38fcead62c90cf07ad
W4v3-cmd <charlymayeur3@gmail.com>
2025-01-03 00:12:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ window.addEventListener("scroll", () => {
     const searchBarContainer = document.querySelector(".search-bar-container");
 
     if (window.scrollY > 30) {
-        // Réduire le logo et le déplacer en haut à gauche
+        
         logoContainer.classList.add("logo-small");
-        // Fixer le conteneur sous le logo
+        
         searchBarContainer.classList.add("fixed-container");
         textBvn.classList.add("rmvBvn");
     } else {
-        // Restaurer la position initiale du logo
+        
         logoContainer.classList.remove("logo-small");
-        // Restaurer la position initiale du conteneur
+        
         searchBarContainer.classList.remove("fixed-container");
         textBvn.classList.remove("rmvBvn");
     }
