49241078c0bbcba9d1ff7894a1393d87637ef4f7
W4v3-cmd <charlymayeur3@gmail.com>
2025-01-03 17:13:46 +0100

    script update

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.addEventListener("scroll", () => {
     const logoContainer = document.querySelector(".main-intro");
     const textElement = document.querySelector(".hidden-text");
     const textBvn = document.querySelector(".bienvenue-txt");
+    const swipeGif = document.querySelector(".swipeGif");
+    const trait = document.querySelector(".trait-intro");
     
     const searchBarContainer = document.querySelector(".search-bar-container");
 
@@ -11,11 +13,13 @@ window.addEventListener("scroll", () => {
         
         searchBarContainer.classList.add("fixed-container");
         textBvn.classList.add("rmvBvn");
+        swipeGif.classList.add("rmvSwipeGif");
     } else {
         
         logoContainer.classList.remove("logo-small");
         
         searchBarContainer.classList.remove("fixed-container");
         textBvn.classList.remove("rmvBvn");
+        swipeGif.classList.remove("rmvSwipeGif");
     }
 });
