ba875f0e18baef9f2cdeaccc8c33667482e3ed59
lyxorl <orlanmonjoly@gmail.com>
2025-01-15 16:14:09 +0100

    ajout de tout les bouttons

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ window.addEventListener("scroll", () => {
     const textBvn = document.querySelector(".bienvenue-txt");
     const swipeGif = document.querySelector(".swipeGif");
     const trait = document.querySelector(".trait-intro");
-    const sectionBureau = document.getElementById("section-bureau");
+
+    const sectionBureau = document.getElementById("section-bureau"); //mettre ces variables en global pour gain de temps ?
     const sectionEntreprise = document.getElementById("section-entreprise");
+    const sectionSoiree = document.getElementById("section-soiree");
+    const sectionEvent = document.getElementById("section-event");
+    const sectionVoyage = document.getElementById("section-voyage");
+    const sectionCommunication = document.getElementById("section-communication");
+    const sectionPolyvalent = document.getElementById("section-polyvalent");
     
     const searchBarContainer = document.querySelector(".search-bar-container");
 
@@ -25,13 +31,26 @@ window.addEventListener("scroll", () => {
 
         sectionBureau.classList.remove("active");
         sectionEntreprise.classList.remove("active");
+        sectionCommunication.classList.remove("active");
+        sectionEvent.classList.remove("active");
+        sectionSoiree.classList.remove("active");
+        sectionVoyage.classList.remove("active");
+        sectionPolyvalent.classList.remove("active");
     }
 });
+
+//il existe surement un moyen de simplifier les boutton suivant a regarder ...
 
 document.getElementById("bureau-button").addEventListener("click", () => {
     const section = document.getElementById("section-bureau");
     section.classList.toggle("active");
     document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
+
 
 });
 
@@ -39,5 +58,70 @@ document.getElementById("entreprise-button").addEventListener("click", () => {
     const section = document.getElementById("section-entreprise");
     section.classList.toggle("active");
     document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
+
+});
+
+document.getElementById("soiree-button").addEventListener("click", () => {
+    const section = document.getElementById("section-soiree");
+    section.classList.toggle("active");
+    document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
+
+});
+
+document.getElementById("event-button").addEventListener("click", () => {
+    const section = document.getElementById("section-event");
+    section.classList.toggle("active");
+    document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
+
+});
+
+document.getElementById("voyage-button").addEventListener("click", () => {
+    const section = document.getElementById("section-voyage");
+    section.classList.toggle("active");
+    document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+
+});
+
+document.getElementById("communication-button").addEventListener("click", () => {
+    const section = document.getElementById("section-communication");
+    section.classList.toggle("active");
+    document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-polyvalent").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
+
+});
+
+document.getElementById("polyvalent-button").addEventListener("click", () => {
+    const section = document.getElementById("section-polyvalent");
+    section.classList.toggle("active");
+    document.getElementById("section-entreprise").classList.remove("active");
+    document.getElementById("section-bureau").classList.remove("active");
+    document.getElementById("section-event").classList.remove("active");
+    document.getElementById("section-soiree").classList.remove("active");
+    document.getElementById("section-communication").classList.remove("active");
+    document.getElementById("section-voyage").classList.remove("active");
 
 });
